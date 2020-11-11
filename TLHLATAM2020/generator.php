aecd7cc808f48f2fe0eefea5212da633e5243200
RicardoNaVi <ricardo.navarro@talent-network.org>
2020-11-11 12:27:58 -0600

    Certificado LATAM participantes DONE

---
+++ TLHLATAM2020/generator.php
@@ -0,0 +1,54 @@
+<?php
+	$Table = "LATAM";
+	$servername = "localhost";
+	$username = "talentne_2020ah";
+	$password = "RnNxO=T56!{r";
+	$dbname = "talentne_2020certifathome";
+
+	ini_set('max_execution_time', 0);
+	mb_internal_encoding('UTF-8');
+	mb_http_output('UTF-8');
+	ini_set('default_charset', 'utf-8');
+
+	$pedido = $_POST['pedido'];
+	$email = mb_strtolower($_POST['email']);
+	
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$sql = "SELECT id,Email,UPPER(Nombre) as nameP,Downloads FROM $Table WHERE email='$email' AND id= '$pedido'";
+	$resultado = $conn->query($sql);
+	$code = "0";
+	if ($resultado -> num_rows > 0) {
+		try {
+			$row = $resultado -> fetch_assoc();
+			$sql = "UPDATE $Table SET Downloads=Downloads+1 WHERE email='$email' AND id= '$pedido'";
+			$conn -> query($sql);
+			header('Content-Type: image/png');
+			header ( "Content-Type: application/force-download" );
+			header ( "Content-Type: application/octet-stream" );
+			header ( "Content-Type: application/download" );
+			header ( "Content-Type: png" );
+			header ( "Content-Disposition: attachment; filename=certificado.png" );
+			header ( "Content-Transfer-Encoding: binary" );
+			header ( "Accept-Ranges: bytes" );
+			$im     = imagecreatefrompng("Certificado.png");
+			$color = imagecolorallocate($im, 0, 0, 0);
+			$px     = (imagesx($im) - 7.5 * strlen($row["nameP"])) / 2;
+			//$fuente = "nutmegregular.ttf";
+			$fuente = realpath('font/nutmegregular.ttf');
+			$fontwidth = 5;
+			$center = (imagesx($im) / 2) - ($fontwidth * (strlen($row["nameP"])/2))*14;
+			imagettftext($im, 80, 0, $center, 1120, $color, $fuente, $row["nameP"]);
+			imagepng($im);
+			$code = "0";
+		} catch (Exception $e) {
+			//throw $th;
+			die($e);
+		}	
+	}else{
+		$code = "1";
+	}
+	echo $code;
+?>
---
+++ TLHLATAM2020/validity.php
@@ -0,0 +1,26 @@
+<?php
+	$Table = "LATAM";
+	$servername = "localhost";
+	$username = "talentne_2020ah";
+	$password = "RnNxO=T56!{r";
+	$dbname = "talentne_2020certifathome";
+
+	$pedido = $_POST['pedido'];
+	$email = mb_strtolower($_POST['email']);
+
+	$code = "0";
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$sql = "SELECT id,Email,UPPER(Nombre) as nameP,Downloads FROM $Table WHERE email='$email' AND id = '$pedido'";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		global $code;
+		$code = "0";
+	}else{
+		global $code;
+		$code = "1";
+	}
+	echo $code;
+?>

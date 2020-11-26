6e32ead45ea1d6b50b4e1388dc296af53ada4c5f
RicardoNaVi <ricardo.navarro@talent-network.org>
2020-11-26 08:43:57 -0600

    LATAM FINISHED

--- TLHLATAMSpeaker2020/generator.php
+++ TLHLATAMSpeaker2020/generator.php
@@ -1,15 +1,5 @@
 <?php
-	function RemoveSpecialChar($str) { 
-		$raro = array("á","Á","é","É","í","Í","ó","Ó","ú","Ú","ñ","Ñ");
-		$normal = array("a","A","e","E","i","I","o","O","u","U","n","N");
-		// Using str_replace() function  
-		// to replace the word  
-		$res = str_replace($raro, $normal, $str);
-		
-		// Returning the result  
-		return $res; 
-    } 
-	$Table = "LATAM";
+	$Table = "LATAMSpeaker";
 	$servername = "localhost";
 	$username = "talentne_2020ah";
 	$password = "RnNxO=T56!{r";
@@ -20,21 +10,21 @@
 	mb_http_output('UTF-8');
 	ini_set('default_charset', 'utf-8');
 
-	$pedido = $_POST['pedido'];
+	$name = mb_strtoupper($_POST['nombre']);
+	$email = mb_strtolower($_POST['email']);
 	
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
-	$sql = "SELECT id,Email,UPPER(Nombre) as nameP,Downloads FROM $Table WHERE id= '$pedido'";
+	$sql = "SELECT Email,UPPER(Nombre_Ponente) as nameP,Panel,Downloads FROM $Table WHERE email='$email' AND UPPER(Nombre_Ponente) LIKE '%$name%'";
 	$resultado = $conn->query($sql);
 	$code = "0";
-	if ($resultado->num_rows > 0) {
+	if ($resultado -> num_rows > 0) {
 		try {
 			$row = $resultado -> fetch_assoc();
-			$sql = "UPDATE $Table SET Downloads=Downloads+1 WHERE id= '$pedido'";
+			$sql = "UPDATE $Table SET Downloads=Downloads+1 WHERE email='$email' AND UPPER(Nombre_Ponente) LIKE '%$name%'";
 			$conn -> query($sql);
-			//echo ($row["nameP"]);
 			header('Content-Type: image/png');
 			header ( "Content-Type: application/force-download" );
 			header ( "Content-Type: application/octet-stream" );
@@ -51,8 +41,7 @@
 			$font = imageloadfont($fuente);
 			$fontwidth = 5;
 			$center = (imagesx($im) / 2) - ($fontwidth * (strlen($row["nameP"])/2))*14;
-			//imagestring ($im, $font , $center , 1120 , $row["nameP"] , $color );
-			imagettftext($im, 80, 0, $center, 1120, $color, $fuente, RemoveSpecialChar($row["nameP"]));
+			imagettftext($im, 80, 0, $center, 1340, $color, $fuente, $row["nameP"]);
 			imagepng($im);
 			$code = "0";
 		} catch (Exception $e) {

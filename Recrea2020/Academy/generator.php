e49f86d1929d94b059f6c5ce83b90720cfa06f5f
RicardoNaVi <ricardo.navarro@talent-network.org>
2020-12-09 13:28:31 -0600

    UTF solved

--- Recrea2020/Academy/generator.php
+++ Recrea2020/Academy/generator.php
@@ -1,7 +1,7 @@
 <?php
 	function RemoveSpecialChar($str) { 
-		$raro = array("á","Á","é","É","í","Í","ó","Ó","ú","Ú","ñ","Ñ");
-		$normal = array("a","A","e","E","i","I","o","O","u","U","n","N");
+		$raro = array("á","Á","é","É","í","Í","ó","Ó","ú","Ú","ñ","Ñ","?");
+		$normal = array("a","A","e","E","i","I","o","O","u","U","n","N","");
 		$res = str_replace($raro, $normal, $str);
 		return $res; 
 	} 
@@ -10,19 +10,13 @@
 	$servername = "localhost";
 	$username = "recrea_202";
 	$password = "ziIy$06Hz$3C";
-	$dbname = "recrea_202";
-
-	ini_set('max_execution_time', 0);
-	mb_internal_encoding('UTF-8');
-	mb_http_output('UTF-8');
-	ini_set('default_charset', 'utf-8');
+	$dbname = "recrea_academy_certificados";
 
 	$pedido = $_POST['pedido'];
-	
-	error_reporting(E_ERROR | E_PARSE);
 
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+	$conn ->set_charset("utf8");
 	$sql = "SELECT id,Email,UPPER(Name) as nameP,Downloads FROM $Table WHERE id= '$pedido'";
 	$resultado = $conn->query($sql);
 	$code = "0";
@@ -31,10 +25,7 @@
 			$row = $resultado -> fetch_assoc();
 			$sql = "UPDATE $Table SET Downloads=Downloads+1 WHERE id= '$pedido'";
 			$conn -> query($sql);
-			$nameP = $row["nameP"];
-			//$nameP = RemoveSpecialChar($row["nameP"]);
-			echo $nameP;
-			var_dump($resultado);
+			$nameP = RemoveSpecialChar($row["nameP"]);
 			header('Content-Type: image/png');
 			header ( "Content-Type: application/force-download" );
 			header ( "Content-Type: application/download" );
@@ -43,14 +34,13 @@
 			$im     = imagecreatefrompng("img/Certificado.png");
 			$color = imagecolorallocate($im, 0, 0, 0);
 			$px     = (imagesx($im) - 7.5 * strlen($row["nameP"])) / 2;
-			$fuente = realpath('font/nutmegregular.ttf');
-			$font = imageloadfont($fuente);
 			$fontwidth = 5;
 			$center = (imagesx($im) / 2) - ($fontwidth * (strlen($row["nameP"])/2))*14;
-			imagettftext($im, 80, 0, $center, 1620, $color, $fuente,$nameP);
+			imagettftext($im, 80, 0, $center, 1620, $color, realpath("font/nutmegregular.ttf"),$nameP);
 			imagepng($im);
 			$code = "0";
 		} catch (Exception $e) { die($e); }	
 	}else{ $code = "1"; }
 	echo $code;
+	$conn->close();
 ?>
--- Recrea2020/Familia/generator.php
+++ Recrea2020/Familia/generator.php
@@ -12,17 +12,11 @@
 	$password = "0bv$tr16dG7*SRCfB2C91t";
 	$dbname = "wp_recrea_familia";
 
-	ini_set('max_execution_time', 0);
-	mb_internal_encoding('UTF-8');
-	mb_http_output('UTF-8');
-	ini_set('default_charset', 'utf-8');
-
 	$pedido = $_POST['pedido'];
 	
-	error_reporting(E_ERROR | E_PARSE);
-
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+	$conn ->set_charset("utf8");
 	$sql = "SELECT id,Email,UPPER(Name) as nameP,Downloads FROM $Table WHERE id= '$pedido'";
 	$resultado = $conn->query($sql);
 	$code = "0";
@@ -31,10 +25,7 @@
 			$row = $resultado -> fetch_assoc();
 			$sql = "UPDATE $Table SET Downloads=Downloads+1 WHERE id= '$pedido'";
 			$conn -> query($sql);
-			$nameP = $row["nameP"];
-			//$nameP = RemoveSpecialChar($row["nameP"]);
-			echo $nameP;
-			var_dump($resultado);
+			$nameP = RemoveSpecialChar($row["nameP"]);
 			header('Content-Type: image/png');
 			header ( "Content-Type: application/force-download" );
 			header ( "Content-Type: application/download" );
@@ -43,11 +34,9 @@
 			$im     = imagecreatefrompng("img/Certificado.png");
 			$color = imagecolorallocate($im, 0, 0, 0);
 			$px     = (imagesx($im) - 7.5 * strlen($row["nameP"])) / 2;
-			$fuente = realpath('font/nutmegregular.ttf');
-			$font = imageloadfont($fuente);
 			$fontwidth = 5;
 			$center = (imagesx($im) / 2) - ($fontwidth * (strlen($row["nameP"])/2))*14;
-			imagettftext($im, 80, 0, $center, 1620, $color, $fuente,$nameP);
+			imagettftext($im, 80, 0, $center, 1620, $color, realpath("font/nutmegregular.ttf"),$nameP);
 			imagepng($im);
 			$code = "0";
 		} catch (Exception $e) { die($e); }	

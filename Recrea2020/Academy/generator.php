4c1cbe5cf88fd78b0949a51247de221dde4da321
RicardoNaVi <ricardo.navarro@talent-network.org>
2020-12-10 11:51:44 -0600

    center fixed

--- Recrea2020/Academy/generator.php
+++ Recrea2020/Academy/generator.php
@@ -5,7 +5,6 @@
 		$res = str_replace($raro, $normal, $str);
 		return $res; 
 	} 
-	
 	$Table = "academy";
 	$servername = "10.9.4.40";
 	$username = "recrea_2020";
@@ -25,18 +24,17 @@
 			$row = $resultado -> fetch_assoc();
 			$sql = "UPDATE $Table SET Downloads=Downloads+1 WHERE id= '$pedido'";
 			$conn -> query($sql);
-			$nameP = RemoveSpecialChar($row["nameP"]);
 			header('Content-Type: image/png');
 			header ( "Content-Type: application/force-download" );
 			header ( "Content-Type: application/download" );
 			header ( "Content-Type: png" );
 			header ( "Content-Disposition: attachment; filename=certificado.png" );
 			$im     = imagecreatefrompng("img/Certificado.png");
+			$fontwidth = 80;
+			$nameP = RemoveSpecialChar($row["nameP"]);
+			$center = (imagesx($im) / 2) - (($fontwidth-10) * (strlen($nameP)))/2;
 			$color = imagecolorallocate($im, 0, 0, 0);
-			$px     = (imagesx($im) - 7.5 * strlen($row["nameP"])) / 2;
-			$fontwidth = 5;
-			$center = (imagesx($im) / 2) - ($fontwidth * (strlen($row["nameP"])/2))*14;
-			imagettftext($im, 80, 0, $center, 1620, $color, realpath("font/nutmegregular.ttf"),$nameP);
+			imagettftext($im, $fontwidth, 0, $center, 1620, $color, realpath("font/nutmegregular.ttf"),$nameP);
 			imagepng($im);
 			$code = "0";
 		} catch (Exception $e) { die($e); }	

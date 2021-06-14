a2a5db98b75a40d41ddd7f05fbcb08574b644bf2
Ricardo N <ricardo.navarro@talent-network.org>
2021-06-14 13:46:26 -0500

    Digital certificates

---
+++ AtHomes2021/Digital/SPEAKER/validity.php
@@ -0,0 +1,23 @@
+<?php
+	$Table = "DigitalSpeaker";
+	$servername = "localhost";
+	$username = "uhfqd5vcyivsh";
+	$password = "mwssmgg8b8qk";
+	$dbname = "dblazobsushgcp";
+
+	$pedido = $_POST['pedido'];
+
+	$code = "0";
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+	$sql = "SELECT id,email,UPPER(name) as nameP,Downloads FROM $Table WHERE id = '$pedido'";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		global $code;
+		$code = "0";
+	}else{
+		global $code;
+		$code = "1";
+	}
+	echo $code;
+?>

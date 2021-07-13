0335c462b5449fa129234ab1dd8a8dda26794c75
RicardoNaVi <ricardo.navarro@talent-network.org>
2021-07-13 14:03:32 -0500

    Certificados Recrea

---
+++ Recrea/Recrea2021/Participante/generator.php
@@ -0,0 +1,44 @@
+<?php
+	function RemoveSpecialChar($str) { 
+		$raro = array("á","Á","é","É","í","Í","ó","Ó","ú","Ú","ñ","Ñ","?");
+		$normal = array("á","Á","é","É","í","Í","ó","Ó","ú","Ú","ñ","Ñ","?");
+		$res = str_replace($raro, $normal, $str);
+		return $res; 
+	} 
+	$Table = "RecreaParticipantes2021";
+	$servername = "localhost";
+	$username = "uwdgjqube5wvj";
+	$password = "mw1ucrn81jy0";
+	$dbname = "dbvgisqd7bnkzg";
+
+	$pedido = $_POST['pedido'];
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+	$conn ->set_charset("utf8");
+	$sql = "SELECT id,email,UPPER(name) as nameP,Downloads FROM $Table WHERE id= '$pedido'";
+	$resultado = $conn->query($sql);
+	$code = "0";
+	if ($resultado->num_rows > 0) {
+		try {
+			$row = $resultado -> fetch_assoc();
+			$sql = "UPDATE $Table SET Downloads=Downloads+1 WHERE id= '$pedido'";
+			$conn -> query($sql);
+			header('Content-Type: image/png');
+			header ( "Content-Type: application/force-download" );
+			header ( "Content-Type: application/download" );
+			header ( "Content-Type: png" );
+			header ( "Content-Disposition: attachment; filename=Certificado.png" );
+			$im     = imagecreatefrompng("img/Certificado.png");
+			$fontwidth = 5;
+			$nameP = RemoveSpecialChar($row["nameP"]);
+			$center = (imagesx($im) / 2) - ($fontwidth * (strlen($row["nameP"])/2))*14;
+			$color = imagecolorallocate($im, 0, 0, 0);
+			imagettftext($im, 80, 0, $center, 1490, $color, realpath("font/nutmegregular.ttf"),$nameP);
+			imagepng($im);
+			$code = "0";
+		} catch (Exception $e) { die($e); }	
+	}else{ $code = "1"; }
+	echo $code;
+	$conn->close();
+?>

11c55842786fce492d37f576390386641d5f6f96
RicardoNaVi <ricardo.navarro@talent-network.org>
2021-07-13 14:17:37 -0500

    Digital hack

---
+++ AtHomes2021/Digital/HACK/validity.php
@@ -0,0 +1,25 @@
+<?php
+	$Table = "DigitalHack";
+	$servername = "localhost";
+	$username = "uhfqd5vcyivsh";
+	$password = "mwssmgg8b8qk";
+	$dbname = "dblazobsushgcp";
+
+	$email = mb_strtolower($_POST['email']);
+
+	$code = "0";
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$sql = "SELECT Email,UPPER(Nombre_Ponente) as nameP,Panel,Downloads FROM $Table WHERE email='$email'";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		global $code;
+		$code = "0";
+	}else{
+		global $code;
+		$code = "1";
+	}
+	echo $code;
+?>
